eb6359788b5fe2fe7b4152b84815a67197bb548d
GisellaMoreno7 <gisellamoreno2002@gmail.com>
2024-11-15 00:24:08 -0300

    Estructuras puestas en función admin

--- vista/admin/listarUsuario.php
+++ vista/admin/listarUsuario.php
@@ -1,25 +1,6 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Librerías -->
-    <script src="../js/jquery-3.7.1.min.js"></script>
-    <script src="../js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/bootstrap-icons-1.10.5/font/bootstrap-icons.min.css">
-
-    <!-- SweetAlert CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <!-- SweetAlert JS -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
-
-    <title>Listar usuarios</title>
-</head>
 <?php
-
-include_once('../../configuracion.php');
+include_once("../../configuracion.php");
+include_once(ROOT_PATH . "vista/estructura/header.php");
 $session = new Session();
 
 // Incluyo modales
@@ -28,7 +9,6 @@ include_once('modificarUsuario.php');
 include_once('bajaUsuario.php');
 include_once('altaUsuario.php');
 include_once('modificarRoles.php');
-
 
 // Creo instancia del objeto AbmUsuario y accedo al método correspondiente
 $objUsuario = new AbmUsuario();
@@ -42,140 +22,128 @@ if (count($colUsuarios) > 0) {
 
 ?>
 
-<body>
-    <h3> Iniciado como <?php echo $_SESSION['usnombre'] ?></h3>
-    <?php if (isset($mensaje)) : ?>
-        <script>
-            // Mostrar notificación de SweetAlert
-            Swal.fire({
-                title: "<?php echo $mensaje ?>",
-                icon: "<?php echo $icono ?>"
-            });
-        </script>
-    <?php endif; ?>
-    <div class="justify-content-md-center align-items-center mt-5">
-        <div class="card shadow  mx-usuario">
-            <div class="card-header">
-                <h3>Listado de usuarios cargados en la base de datos</h3>
-                <div class="d-flex">
+<h3> Iniciado como <?php echo $_SESSION['usnombre'] ?></h3>
+<div class="justify-content-md-center align-items-center mt-5">
+    <div class="card shadow  mx-usuario">
+        <div class="card-header">
+            <h3>Listado de usuarios cargados en la base de datos</h3>
+            <div class="d-flex">
 
-                    <button class="altaUsuario btn btn-success" type="button"
-                        data-bs-toggle="modal"
-                        data-bs-target="#altaUsuario">
-                        Crear usuario
-                    </button>
+                <button class="altaUsuario btn btn-success" type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#altaUsuario">
+                    Crear usuario
+                </button>
 
-                    <button class="altaRol btn btn-success me-2" type="button"
-                        data-bs-toggle="modal"
-                        data-bs-target="#altaRol">
-                        Añadir rol
-                    </button>
+                <button class="altaRol btn btn-success me-2" type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#altaRol">
+                    Añadir rol
+                </button>
 
-                    <a href="../login/accion/cerrarSesion.php">
-                        <input type="submit" class="btn btn-secondary me-2" value="Cerrar sesión">
-                    </a>
-                </div>
-            </div>
-            <div class="card-body">
-                <?php if ($hayUsuarios): ?>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">ID usuario</th>
-                                <th scope="col">Nombre usuario</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Rol</th>
-                                <th scope="col">Deshabilitado</th>
-                                <th scope="col">Acciones</th>
-                            </tr>
-                        </thead>
-                        <?php
-                        for ($i = 0; $i < count($colUsuarios); $i++):
-                            $idusuario = $colUsuarios[$i]->getIdUsuario();
-                            $usnombre = $colUsuarios[$i]->getUsNombre();
-                            $usmail = $colUsuarios[$i]->getUsMail();
-
-                            // Creo instancia del objeto AbmUsuarioRol para mostrar los roles de los usuarios
-                            $objUsuarioRol = new AbmUsuarioRol();
-
-                            $usuario = ['idusuario' => $idusuario];
-                            // $usuario = ['idusuario' => 1];
-                            $colUsuarioRol = $objUsuarioRol->buscar($usuario);
-                            $cantRoles = count($colUsuarioRol);
-
-                            // Creamos array para guardar los roles del usuario
-                            $colRoles = [];
-
-                            // Recorremos todos los roles del usuario
-                            if ($cantRoles >= 1) {
-                                foreach ($colUsuarioRol as $rol) {
-                                    $roldescripcion = $rol->getObjRol()->getRolDescripcion();
-                                    $colRoles[] = $roldescripcion;
-                                }
-
-                                // Separamos con una "coma" en >> caso de que un usuario tenga más de un rol <<
-                                $roles = implode(", ", $colRoles);
-                            } else {
-                                $roles = "-";
-                            }
-
-                            $usdeshabilitado = $colUsuarios[$i]->getUsDeshabilitado();
-                            if ($usdeshabilitado == '0000-00-00 00:00:00') {
-                                $usdeshabilitado = "Activo";
-                            }
-                        ?>
-                            <tbody>
-                                <tr>
-                                    <td><?php echo $idusuario ?></td>
-                                    <td><?php echo $usnombre ?></td>
-                                    <td><?php echo $usmail ?></td>
-                                    <td><?php echo $roles ?></td>
-                                    <td><?php echo $usdeshabilitado ?></td>
-                                    <td>
-                                        <?php if ($usdeshabilitado == "Activo") : ?>
-                                            <div class="d-flex">
-                                                <button class="modificarRoles btn btn-primary" type="button"
-                                                    data-idusuario="<?php echo $idusuario; ?>"
-                                                    data-usnombre="<?php echo $usnombre; ?>"
-                                                    data-roles="<?php echo $roles; ?>"
-                                                    data-bs-toggle="modal"
-                                                    data-bs-target="#modificarRoles">
-                                                    Modificar roles
-                                                </button>
-
-                                                <button class="modificarUsuario btn btn-primary" type="button"
-                                                    data-idusuario="<?php echo $idusuario; ?>"
-                                                    data-usnombre="<?php echo $usnombre; ?>"
-                                                    data-usmail="<?php echo $usmail; ?>"
-                                                    data-bs-toggle="modal"
-                                                    data-bs-target="#modificarUsuario">
-                                                    Modificar datos
-                                                </button>
-
-                                                <button class="bajaUsuario btn btn-danger" type="button"
-                                                    data-idusuario="<?php echo $idusuario; ?>"
-                                                    data-usnombre="<?php echo $usnombre; ?>"
-                                                    data-roles="<?php echo $roles; ?>"
-                                                    data-bs-toggle="modal"
-                                                    data-bs-target="#bajaUsuario">
-                                                    Deshabilitar
-                                                </button>
-
-                                            </div>
-                                        <?php endif ?>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        <?php endfor; ?>
-                    </table>
-                <?php else: ?>
-                    <p><?php echo "No hay usuarios cargados en la base de datos."; ?></p>
-                <?php endif; ?>
+                <a href="../login/accion/cerrarSesion.php">
+                    <input type="submit" class="btn btn-secondary me-2" value="Cerrar sesión">
+                </a>
             </div>
         </div>
-    </div>
+        <div class="card-body">
+            <?php if ($hayUsuarios): ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID usuario</th>
+                            <th scope="col">Nombre usuario</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Rol</th>
+                            <th scope="col">Deshabilitado</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <?php
+                    for ($i = 0; $i < count($colUsuarios); $i++):
+                        $idusuario = $colUsuarios[$i]->getIdUsuario();
+                        $usnombre = $colUsuarios[$i]->getUsNombre();
+                        $usmail = $colUsuarios[$i]->getUsMail();
 
-</body>
+                        // Creo instancia del objeto AbmUsuarioRol para mostrar los roles de los usuarios
+                        $objUsuarioRol = new AbmUsuarioRol();
+
+                        $usuario = ['idusuario' => $idusuario];
+                        // $usuario = ['idusuario' => 1];
+                        $colUsuarioRol = $objUsuarioRol->buscar($usuario);
+                        $cantRoles = count($colUsuarioRol);
+
+                        // Creamos array para guardar los roles del usuario
+                        $colRoles = [];
+
+                        // Recorremos todos los roles del usuario
+                        if ($cantRoles >= 1) {
+                            foreach ($colUsuarioRol as $rol) {
+                                $roldescripcion = $rol->getObjRol()->getRolDescripcion();
+                                $colRoles[] = $roldescripcion;
+                            }
+
+                            // Separamos con una "coma" en >> caso de que un usuario tenga más de un rol <<
+                            $roles = implode(", ", $colRoles);
+                        } else {
+                            $roles = "-";
+                        }
+
+                        $usdeshabilitado = $colUsuarios[$i]->getUsDeshabilitado();
+                        if ($usdeshabilitado == '0000-00-00 00:00:00') {
+                            $usdeshabilitado = "Activo";
+                        }
+                    ?>
+                        <tbody>
+                            <tr>
+                                <td><?php echo $idusuario ?></td>
+                                <td><?php echo $usnombre ?></td>
+                                <td><?php echo $usmail ?></td>
+                                <td><?php echo $roles ?></td>
+                                <td><?php echo $usdeshabilitado ?></td>
+                                <td>
+                                    <?php if ($usdeshabilitado == "Activo") : ?>
+                                        <div class="d-flex">
+                                            <button class="modificarRoles btn btn-primary" type="button"
+                                                data-idusuario="<?php echo $idusuario; ?>"
+                                                data-usnombre="<?php echo $usnombre; ?>"
+                                                data-roles="<?php echo $roles; ?>"
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#modificarRoles">
+                                                Modificar roles
+                                            </button>
+
+                                            <button class="modificarUsuario btn btn-primary" type="button"
+                                                data-idusuario="<?php echo $idusuario; ?>"
+                                                data-usnombre="<?php echo $usnombre; ?>"
+                                                data-usmail="<?php echo $usmail; ?>"
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#modificarUsuario">
+                                                Modificar datos
+                                            </button>
+
+                                            <button class="bajaUsuario btn btn-danger" type="button"
+                                                data-idusuario="<?php echo $idusuario; ?>"
+                                                data-usnombre="<?php echo $usnombre; ?>"
+                                                data-roles="<?php echo $roles; ?>"
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#bajaUsuario">
+                                                Deshabilitar
+                                            </button>
+
+                                        </div>
+                                    <?php endif ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    <?php endfor; ?>
+                </table>
+            <?php else: ?>
+                <p><?php echo "No hay usuarios cargados en la base de datos."; ?></p>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
 
 <script src="../js/md5.js"></script>
 <script src="../js/verPass.js"></script>
@@ -185,4 +153,5 @@ if (count($colUsuarios) > 0) {
 <script src="../js/ajax/modificarUsuario.js"></script>
 <script src="../js/ajax/altaUsuario.js"></script>
 
-</html>
+<?php
+include_once(ROOT_PATH . "vista/estructura/footer.php");
--- vista/login/formCrearCuenta.php
+++ vista/login/formCrearCuenta.php
@@ -1,25 +1,7 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- SweetAlert CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <!-- SweetAlert JS -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
-
-
-    <!-- Librerías -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/bootstrap-icons-1.10.5/font/bootstrap-icons.min.css">
-    <script src="../js/jquery-3.7.1.min.js"></script>
-    <script src="../js/bootstrap.bundle.min.js"></script>
-
-    <script src="../js/encriptar.js"></script>
-    <title>Crear cuenta</title>
-</head>
 <?php
+include_once("../../configuracion.php");
+include_once(ROOT_PATH . "vista/estructura/header.php");
+
 // Este paso es para mostrar notificación de SweetAlert en caso de haber realizado alguna acción
 if (isset($_COOKIE['mensaje'])) {
     $mensaje = $_COOKIE['mensaje'];
@@ -29,45 +11,44 @@ if (isset($_COOKIE['mensaje'])) {
     setcookie("mensaje", "", time() - 3600, "/");
     setcookie("icono", "", time() - 3600, "/");
 }
+
 ?>
+<?php if (isset($mensaje)) : ?>
+    <script>
+        // Mostrar notificación de SweetAlert
+        Swal.fire({
+            title: "<?php echo $mensaje ?>",
+            icon: "<?php echo $icono ?>"
+        });
+    </script>
+<?php endif; ?>
+<form id="form" name="form" method="post" onsubmit="encriptar()" action="accion/crearCuenta.php">
+    <h3>Crear cuenta</h3>
+    <label for="usnombre">Nombre usuario</label>
+    <input type="text" class="form-control" id="usnombre" name="usnombre">
 
-<body>
-    <?php if (isset($mensaje)) : ?>
-        <script>
-            // Mostrar notificación de SweetAlert
-            Swal.fire({
-                title: "<?php echo $mensaje ?>",
-                icon: "<?php echo $icono ?>"
-            });
-        </script>
-    <?php endif; ?>
-    <form id="form" name="form" method="post" onsubmit="encriptar()" action="accion/crearCuenta.php">
-        <h3>Crear cuenta</h3>
-        <label for="usnombre">Nombre usuario</label>
-        <input type="text" class="form-control" id="usnombre" name="usnombre">
+    <label for="usmail">Email</label>
+    <input type="text" class="form-control" id="usmail" name="usmail">
 
-        <label for="usmail">Email</label>
-        <input type="text" class="form-control" id="usmail" name="usmail">
+    <label for="uspass">Contraseña</label>
+    <div class="input-group">
+        <input type="password" class="form-control" id="uspass" name="uspass">
+        <button type="button" class="btn btn-secondary"
+            onmousedown="mostrarPass(true)"
+            onmouseup="mostrarPass(false)"
+            onmouseleave="mostrarPass(false)">
+            <i id="toggleIcon" class="bi bi-eye-slash"></i>
+        </button>
+    </div>
 
-        <label for="uspass">Contraseña</label>
-        <div class="input-group">
-            <input type="password" class="form-control" id="uspass" name="uspass">
-            <button type="button" class="btn btn-secondary"
-                onmousedown="mostrarPass(true)"
-                onmouseup="mostrarPass(false)"
-                onmouseleave="mostrarPass(false)">
-                <i id="toggleIcon" class="bi bi-eye-slash"></i>
-            </button>
-        </div>
-
-        <input type="submit" class="btn btn-success" value="Confirmar">
-        <a href="formIniciarSesion.php">
-            <input type="button" class="btn btn-primary" value="Volver">
-        </a>
-    </form>
-</body>
+    <input type="submit" class="btn btn-success" value="Confirmar">
+    <a href="formIniciarSesion.php">
+        <input type="button" class="btn btn-secondary" value="Volver">
+    </a>
+</form>
 
 <script src="../js/md5.js"></script>
 <script src="../js/verPass.js"></script>
 
-</html>
+<?php
+include_once(ROOT_PATH . "vista/estructura/footer.php");
