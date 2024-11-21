02d8592a054d4b2c0a48e2423cc1f7173938d1b5
abialmiron <abialmiron@gmail.com>
2024-11-21 19:47:08 -0300

    menu

--- vista/estructura/menu.php
+++ vista/estructura/menu.php
@@ -6,28 +6,6 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="menu">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="../vista/">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo BASE_URL; ?>vista/productos.php">Tienda</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Novedades</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contacto</a>
-        </li>
-      </ul>
-      <a href="<?php echo BASE_URL; ?>vista/login/formIniciarSesion.php" class="btn btn-primary ms-3">Iniciar sesión</a>
-
-      <a href="../cliente/carrito.php" class="ms-3">
-        <img src="<?php echo BASE_URL; ?>vista/images/carrito.png" alt="Carrito" class="imgCart me-2" style="height: 2em;">
-        <span>
-        <?php echo isset($_SESSION['numero']) ? $_SESSION['numero'] : 0; ?>
-        </span>
-      </a>
 
     </div>
   </div>
